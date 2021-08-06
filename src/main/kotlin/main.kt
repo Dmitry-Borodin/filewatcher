24a7cd915342efe2569a0b660a9c8d101a2117fe
Dmitry Borodin <git@DmitryBorodin.com>
2021-08-06 18:40:38 +0200

    sample to see performance improved

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,40 +1,34 @@
 import java.io.File
 import java.lang.RuntimeException
+import java.util.*
 
+/**
+ * This is to see performance
+ */
 fun main(args: Array<String>) {
 
-        //given we have a folder with content
-        val folder = File("../../other/intellij-community/")
-        if (!folder.exists()) {
-            throw RuntimeException("coudn't find directory")
-        }
+    //given we have a folder with content
+    val folder = File("../../other/intellij-community/")
+    if (!folder.exists()) {
+        throw RuntimeException("coudn't find directory")
+    }
 
-        println("starting index")
-        //we can start tracking it's content
-        val fileWatcher = FileWatcher()
-        fileWatcher.addToIndex(folder.toPath())
+    println("starting index")
+    println("timestamp " + Date().toString())
+
+    //we can start tracking it's content
+    val fileWatcher = FileWatcher()
+    fileWatcher.addToIndex(folder.toPath())
 
     println("index finished")
-        //now we can get files by word, that is contained in watched folder
-        val foundFiles = fileWatcher.getFilesWithWord("sample")
-        assert(foundFiles.size == 1)
-        assert(foundFiles[0].toAbsolutePath().toString().contains("sample.txt", false)) //this path can be a full path, not related we used to create a file
+    println("timestamp " + Date().toString())
 
-        //if content is modified while on background
-        File(folder.path.toString() + "/sample.txt")
-            .printWriter().use { out ->
-                out.write("some another characters")
-            }
-        //after some short time changes will be noticed and re-indexed
-        Thread.sleep(100)
+    //now we can get files by word, that is contained in watched folder
+    val foundFiles = fileWatcher.getFilesWithWord("val")
+    println("timestamp " + Date().toString())
+    println("files with val amount is ${foundFiles.size}")
+    println("And they are ${foundFiles}")
 
-        //and we see changes in fileWatcher results
-        assert(fileWatcher.getFilesWithWord("sample").isEmpty())
-        assert(fileWatcher.getFilesWithWord("another").isNotEmpty())
-
-        //can stop tracking if we don't need it anymore, to save resources on keeping index up to date
-        fileWatcher.close()
-
-        //cleanup after this sample run
-        folder.deleteRecursively()
+    //can stop tracking if we don't need it anymore, to save resources on keeping index up to date
+    fileWatcher.close()
 }
